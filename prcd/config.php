9e4f12ae00ce57f29830d1edd48c835ec657e250
jersusrlv <jesusrlvrojo@gmail.com>
2023-07-15 19:17:05 -0600

    stripe

---
+++ prcd/config.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "stripe-php-master/init.php";
+
+    $stripedetails = array(
+        "publishabelKey" => "",
+        "secretKey" => ""
+    );
+
+    /Stripe/Stripe::setApiKey($stripedetails['secretKey']);
+?>
