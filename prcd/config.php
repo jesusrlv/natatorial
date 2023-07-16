44696afa2db421a82cbd3448e9371e667fcd6df6
jersusrlv <jesusrlvrojo@gmail.com>
2023-07-15 19:49:12 -0600

    stripe 2

--- prcd/config.php
+++ prcd/config.php
@@ -2,8 +2,8 @@
     require_once "stripe-php-master/init.php";
 
     $stripedetails = array(
-        "publishabelKey" => "",
-        "secretKey" => ""
+        "publishabelKey" => "pk_test_51NUJBsLW97G0onMCGEvK3Sd1BIADMp0jn7EWD3NE4olfQCkrxVxAwW2LKAa7MrKIOIwL3ftR86h5nAyhlx2lhTOp00Z6DruQr5",
+        "secretKey" => "sk_test_51NUJBsLW97G0onMCuhaetVzs75h33CYJosifzV9G6l2SbzWnCBCAHul8w4XoPo5ASrCDVsl57M1oE2cMokSS8NDy00MDmRiCpS"
     );
 
     /Stripe/Stripe::setApiKey($stripedetails['secretKey']);
