756a0cfee46a3a7c9826b5860249047ba3716303
jesusrlv <jesusrlvrojo@gmail.com>
2023-09-06 12:10:46 -0600

    admin casi listo 2

--- admin/prcd/query.php
+++ admin/prcd/query.php
@@ -9,27 +9,34 @@ if(isset($_POST['fecha'])){
     $fechaBusqueda = $_POST['fecha'];
     $annio = substr($fechaBusqueda, 0, 4);
     $mes = substr($fechaBusqueda, 5, 2); 
-    $sqlBusqueda = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = $annio AND MONTH(fecha_reserva) = $mes AND aprobar = 1 ORDER BY fecha_reserva ASC, hora ASC";
-    $resultadoBusqueda = $conn->query($sqlBusqueda);
 
-    $sqlBusqueda2 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = $annio AND MONTH(fecha_reserva) = $mes AND aprobar = 0 ORDER BY fecha_reserva ASC, hora ASC";
+    // approved
+    $sqlBusqueda = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = $annio AND MONTH(fecha_reserva) = $mes AND aprobar = 1 ORDER BY fecha_reserva ASC, hora ASC";
+    $sqlResultToday = $conn->query($sqlBusqueda);
+
+    // waiting
+    $sqlBusqueda2 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = $annio AND MONTH(fecha_reserva) = $mes AND aprobar = 2 ORDER BY fecha_reserva ASC, hora ASC";
     $resultadoBusqueda = $conn->query($sqlBusqueda2);
+
+    // not approved
+    $sqlBusqueda2 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = $annio AND MONTH(fecha_reserva) = $mes AND aprobar = 0 ORDER BY fecha_reserva ASC, hora ASC";
+    $sqlResultToday2 = $conn->query($sqlBusqueda2);
 }
 else{
 
-// today
-$sqlToday = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 1 ORDER BY fecha_reserva ASC, hora";
-$sqlResultToday = $conn->query($sqlToday);
+    // today
+    $sqlToday = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 1 ORDER BY fecha_reserva ASC, hora";
+    $sqlResultToday = $conn->query($sqlToday);
 
-// waiting
-$sqlToday3 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 2 ORDER BY fecha_reserva ASC, hora";
-$resultadoBusqueda = $conn->query($sqlToday3);
+    // waiting
+    $sqlToday3 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 2 ORDER BY fecha_reserva ASC, hora";
+    $resultadoBusqueda = $conn->query($sqlToday3);
 
-// today not approved
-$sqlToday2 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 0 ORDER BY fecha_reserva ASC, hora";
-$sqlResultToday2 = $conn->query($sqlToday2);
+    // today not approved
+    $sqlToday2 = "SELECT * FROM agenda WHERE YEAR(fecha_reserva) = YEAR(CURRENT_DATE()) AND MONTH(fecha_reserva) = MONTH(CURRENT_DATE()) AND aprobar = 0 ORDER BY fecha_reserva ASC, hora";
+    $sqlResultToday2 = $conn->query($sqlToday2);
 
-// 0 - no approved, 1 - approved, 2 - waiting list
+    // 0 - no approved, 1 - approved, 2 - waiting list
 
 }
 
